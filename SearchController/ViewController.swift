b078b5e6ad139bb66fdfbe5bf0f3647eb90c9e3d
James Power <jep@lepoer.us>
2020-04-24 23:03:44 -0400

    Initial Commit

---
+++ SearchController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchController
+//
+//  Created by James Power on 4/24/20.
+//  Copyright © 2020 James Power. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
